d590c5b51bd17f1db9c8761fcc6a12de3b4aa602
XinghaiN <109878892+XinghaiN@users.noreply.github.com>
2023-03-02 00:05:32 -0800

    Update auth.py

--- dev/website/auth.py
+++ dev/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .models import User, Quiz_M, Quiz_TF, Material
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ## means from __init__.py import db
