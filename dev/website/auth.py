a91a4ed7fa7139facb79656d3401108e11b2f92a
Bingzhen Chen <aa841354118@gmail.com>
2023-04-05 21:19:20 -0700

    allow admin to delete questions and flashcards & fix the issue where sign up requirements not satisfied always return to login tab.

--- dev/website/auth.py
+++ dev/website/auth.py
@@ -1,15 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, Quiz_M, Quiz_TF, Material, Score, Note
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ## means from __init__.py import db
+from . import db  ## means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
-from sqlalchemy.sql import func,desc
+from sqlalchemy.sql import func, desc
 from random import shuffle
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import abort
-
-
-
 
 auth = Blueprint('auth', __name__)
 
@@ -32,7 +29,6 @@ def login():
             flash('Username does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
-
 
 
 @auth.route('/sign_up', methods=['GET', 'POST'])
@@ -69,11 +65,10 @@ def sign_up():
             # user will automatically logined after signup
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            
-            return redirect(url_for('auth.choose_lang_mode'))
-            
-    return render_template("login.html", user=current_user, active_tab='tab-2')
 
+            return redirect(url_for('auth.choose_lang_mode'))
+
+    return render_template("login.html", user=current_user, active_tab='tab-2')
 
 
 @auth.route('/logout')
@@ -81,7 +76,6 @@ def sign_up():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
 
 
 @auth.route('/choose_lang_mode', methods=['GET', 'POST'])
@@ -92,16 +86,16 @@ def choose_lang_mode():
         mode = request.form['mode']
 
         if mode == 'quiz':
-            return redirect(url_for('auth.take_quiz',  user=current_user, language=language))
+            return redirect(url_for('auth.take_quiz', user=current_user, language=language))
 
         elif mode == 'study':
             return redirect(url_for('auth.study', language=language, user=current_user))
-    
+
     return render_template("choose_lang_mode.html", user=current_user)
 
 
-
 @auth.route('/study', methods=['GET', 'POST'])
+@login_required
 def study():
     # Get the language from the URL parameter
     language = request.args.get('language', 'Chinese')
@@ -123,6 +117,10 @@ def study():
             res_cards[i].append(allCards.pop())
 
     allNotes = Note.query.filter_by(user_id=current_user.id).order_by(Note.date.desc()).all()
+    for note in allNotes:
+        # chagne timezone
+        tz_delta = timedelta(hours=-7)
+        note.date += tz_delta
 
     return render_template('study.html', user=current_user, cards=res_cards, notes=allNotes, language=language)
 
@@ -184,6 +182,7 @@ def update_note(id, note_content):
 __user_quizzes_multi = {}
 __user_quizzes_tf = {}
 
+
 @auth.route('/quiz_take', methods=['GET', 'POST'])
 @login_required
 def take_quiz():
@@ -192,10 +191,10 @@ def take_quiz():
 
     # Select 10 random quiz questions from the database
     # Use the chosen language to filter the quiz questions using the filter_by() method.
-    quizzes_multi= Quiz_M.query.filter_by(language=language).order_by(func.random()).limit(5).all()
-    quizzes_tf= Quiz_TF.query.filter_by(language=language).order_by(func.random()).limit(5).all()
+    quizzes_multi = Quiz_M.query.filter_by(language=language).order_by(func.random()).limit(5).all()
+    quizzes_tf = Quiz_TF.query.filter_by(language=language).order_by(func.random()).limit(5).all()
 
-    total = 10 * len(quizzes_multi+quizzes_tf)
+    total = 10 * len(quizzes_multi + quizzes_tf)
 
     if request.method == 'POST':
         # Initialize the a list of dictionaries. 
@@ -204,10 +203,10 @@ def take_quiz():
         score = 0
 
         for key, value in user_answers.items():
-            #print(key, value)
+            # print(key, value)
             if key.startswith('answer-'):
                 quiz_id = key.split('-')[1]
-                #print(quiz_id)  # quiz id in database
+                # print(quiz_id)  # quiz id in database
                 if key.endswith('multi'):
                     question = Quiz_M.query.filter_by(id=quiz_id).first()
                     question_text = question.question
@@ -226,10 +225,10 @@ def take_quiz():
             # Add question_text, user_answer and correct_answer to the list.
             # We are saving question_text, user_answer and correct_answer into this list, 
             # so that quiz_result.html can retrive info from it without querying database.
-            quiz_list.append({"question_text": question_text, "user_answer": user_answer, "correct_answer": correct_answer})
+            quiz_list.append(
+                {"question_text": question_text, "user_answer": user_answer, "correct_answer": correct_answer})
 
-
-        # Display the score, correct answer and user's answers.   
+        # Display the score, correct answer and user's answers.
         if len(user_answers) == 0:
             flash('You did not answer any questions, please take the quiz')
             return redirect(url_for('auth.take_quiz', user=current_user, language=language))
@@ -238,13 +237,13 @@ def take_quiz():
             quiz_score = Score(user_id=current_user.id, score=score, date=datetime.now())
             db.session.add(quiz_score)
             db.session.commit()
-            return render_template('quiz_result.html', user=current_user, language=language, quiz_list=quiz_list, score=score, total=total)
-   
+            return render_template('quiz_result.html', user=current_user, language=language, quiz_list=quiz_list,
+                                   score=score, total=total)
+
     # If it's a GET request, display the quiz questions
-    return render_template('quiz_take.html', user=current_user, language=language, quizzes_multi=quizzes_multi, quizzes_tf=quizzes_tf)
+    return render_template('quiz_take.html', user=current_user, language=language, quizzes_multi=quizzes_multi,
+                           quizzes_tf=quizzes_tf)
 
-
-   
 
 # Administer related routes
 @auth.route('/administer_login', methods=['GET', 'POST'])
@@ -270,7 +269,7 @@ def administer_login():
             return render_template("login.html", active_tab='tab-3', user=current_user)
 
     return render_template("login.html", user=current_user)
-       
+
 
 @auth.route('/admin_dashboard', methods=['GET', 'POST'])
 @login_required
@@ -284,25 +283,25 @@ def admin_dashboard():
         # Set default active tab to be user accounts
         tab = request.args.get('tab', 'user-accounts')
 
-        return render_template('admin_dashboard.html', users=users, user=current_user, quiz_m=quiz_m, quiz_tf=quiz_tf, flashcards=flashcards, tab=tab)
+        return render_template('admin_dashboard.html', users=users, user=current_user, quiz_m=quiz_m, quiz_tf=quiz_tf,
+                               flashcards=flashcards, tab=tab)
     else:
         return render_template("login.html", user=current_user)
-    
-    
+
 
 @auth.route('/admin/users/delete/<int:user_id>', methods=['POST'])
 @login_required
 def admin_delete_user(user_id):
-    if current_user.username == 'administer'  and request.method == 'POST' :
+    if current_user.username == 'administer' and request.method == 'POST':
         user = User.query.get(user_id)
         if user:
             db.session.delete(user)
             db.session.commit()
         return redirect(url_for('auth.admin_dashboard'))
     else:
-        return render_template("login.html",user=current_user)
-    
-   
+        return render_template("login.html", user=current_user)
+
+
 @auth.route('/admin/mcq/delete/<int:question_id>', methods=['POST'])
 @login_required
 def admin_delete_multi_question(question_id):
@@ -311,9 +310,9 @@ def admin_delete_multi_question(question_id):
         if question:
             db.session.delete(question)
             db.session.commit()
-    
+
         tab = request.args.get('tab', 'multiple-choice-quiz')
-        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab ))
+        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab))
     else:
         return render_template("login.html", user=current_user)
 
@@ -326,12 +325,11 @@ def admin_delete_tf_question(question_id):
         if question:
             db.session.delete(question)
             db.session.commit()
-    
+
         tab = request.args.get('tab', 'true-or-false-quiz')
-        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab ))
+        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab))
     else:
         return render_template("login.html", user=current_user)
-  
 
 
 @auth.route('/admin/flashcard/delete/<int:flashcard_id>', methods=['POST'])
@@ -342,9 +340,46 @@ def admin_delete_flashcard(flashcard_id):
         if flashcard:
             db.session.delete(flashcard)
             db.session.commit()
-    
+
         tab = request.args.get('tab', 'flashcards')
-        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab ))
+        return redirect(url_for('auth.admin_dashboard', _anchor=tab, tab=tab))
     else:
         return render_template("login.html", user=current_user)
 
+
+@auth.route('/scoreboard', methods=['GET'])
+@login_required
+def scoreboard():
+    # model to templatem, ready to fill in.
+    user_score = {"username": current_user.username, "rank": "None",
+                  "total_score": "None", "newest_score": "None", "newest_time": "None"}
+    all_rows = []
+    rank_table = db.session.query(Score.user_id, func.sum(Score.score).label(
+        "total_score")).group_by(Score.user_id).order_by(desc("total_score")).all()
+    for rank, item in enumerate(rank_table):
+        try:
+            user = User.query.filter_by(id=item[0]).one()
+        except:
+            continue
+        user_score_newest = Score.query.filter_by(
+            user_id=user.id).order_by(Score.date.desc()).first()
+        row = {"username": user.username, "rank": rank + 1,
+               "total_score": item[1], "newest_score": user_score_newest.score, "newest_time": user_score_newest.date}
+        # replace user_score
+        if item[0] == current_user.id:
+            user_score = row
+        all_rows.append(row)
+
+    return render_template('scoreboard.html', user=current_user, user_score=user_score, all_score=all_rows)
+
+
+@auth.route('/scoreboard/look', methods=['GET'])
+@login_required
+def scoreboard_user():
+    # model to templatem, ready to fill in.
+    all_score = []
+    # user be looked
+    user_looked = User.query.filter_by(username=request.args.get('username')).one()
+    all_score = Score.query.filter_by(user_id=user_looked.id).order_by(Score.date.desc()).all()
+
+    return render_template('scoreboard_user.html', user=current_user, user_looked=user_looked, all_score=all_score)
